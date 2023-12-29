9762b0882e8c44ee3475c134519b539805d0651e
Jules Maestro <jlsodai@gmail.com>
2023-12-29 14:24:58 +0000

    add quote page

---
+++ src/app/(pages)/quote/page.tsx
@@ -0,0 +1,17 @@
+import PageTitle from "@/components/PageTitle";
+import React from "react";
+
+const QuotePage = () => {
+  return (
+    <>
+      <PageTitle title="Request for Quote" />
+      <section className="py-16">
+        <div className="container">
+          <h2>Quote Here</h2>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default QuotePage;
