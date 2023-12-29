cabe6994afe5c3dd7bcb772b379b1a9f1fcff8f9
Jules Maestro <jlsodai@gmail.com>
2023-12-29 14:27:54 +0000

    add contact us link to get in touch

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -10,12 +10,14 @@ const HeroSection = () => {
           <h3 className="text-white text-4xl md:text-4xl lg:text-6xl font-bold lg:pr-24 leading-tight -md:mt-16">
             Your Excellent Partner in Health Delivery
           </h3>
-          <Button
-            variant="outline"
-            className="text-white px-12 mt-6 rounded hover:bg-white"
-          >
-            Get in touch
-          </Button>
+          <a href="/contact-us">
+            <Button
+              variant="outline"
+              className="text-white px-12 mt-6 rounded hover:bg-white"
+            >
+              Get in touch
+            </Button>
+          </a>
         </div>
         <img
           src="https://res.cloudinary.com/dmaestro/image/upload/v1703839005/pdelivery_ctu1t9.png"
