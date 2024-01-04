ca3b37b1bd81dfff2af774672e40e8bc58e291e5
Jules Maestro <jlsodai@gmail.com>
2024-01-04 23:21:38 +0000

    update hero

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 
 const HeroSection = () => {
   return (
-    <div className="bg-[url('https://res.cloudinary.com/dmaestro/image/upload/e_blur:700/co_rgb:000,e_colorize:70/v1704407288/courier_k02dpw.jpg')] bg-cover">
+    <div className="bg-gradient-to-r from-orange-900 to-[#FC5500] z-10">
       <div className="container grid md:grid-cols-2 h-full items-center gap-4">
         <div className="text-center md:text-left">
           <h3
