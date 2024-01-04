2d0ba3af0406b2d045897ff1e8814ccd12349008
Jules Maestro <jlsodai@gmail.com>
2024-01-04 23:04:14 +0000

    update quote bg

--- src/app/(pages)/quote/page.tsx
+++ src/app/(pages)/quote/page.tsx
@@ -63,7 +63,7 @@ const QuotePage = () => {
   return (
     <>
       {/* <PageTitle title="Request for quote" /> */}
-      <section className="bg-cover bg-center bg-[url('https://res.cloudinary.com/dmaestro/image/upload/co_rgb:000,e_colorize:75/v1704408989/handshake_jtpvw4.jpg')]">
+      <section className="bg-cover bg-top bg-[url('https://res.cloudinary.com/dmaestro/image/upload/co_rgb:000,e_colorize:75/w_1920,h_600,g_south,c_fill/v1704409263/pen_jlqyen.jpg')]">
         <div className="container h-72 text-white flex items-center">
           <h2 className="text-5xl">
             Request <span className="text-[#FC5500]">for Quote</span>
